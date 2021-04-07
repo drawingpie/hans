95c347f7d9a079c827b8025f46ef2671785b0f3a
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-04-07 17:42:43 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,30 @@
 //메인슬라이드//
-var slide_index = 1;
-       displaySlides(slide_index);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-       function nextSlide(n) {
-           displaySlides(slide_index += n);
-       }
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-       function currentSlide(n) {
-           displaySlides(slide_index = n);
-       }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-       function displaySlides(n) {
-           var i;
-           var slides = document.getElementsByClassName("showSlide");
-           if (n > slides.length) { slide_index = 1 }
-           if (n < 1) { slide_index = slides.length }
-           for (i = 0; i < slides.length; i++) {
-               slides[i].style.display = "none";
-           }
-           slides[slide_index - 1].style.display = "block";
-       }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 //마우스오버//
 function openMENU(evt, menuName) {
