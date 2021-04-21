a397ce472eb77ceb79bf0be676af9765cb59097d
drawingpie <74900663+drawingpie@users.noreply.github.com>
2021-04-21 18:04:02 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,31 +5,6 @@ $(document).ready(function() {
     prevArrow: $('.prev'),
   });
 });
-//모바일메뉴//
-  $(function() {
-    function slideMenu() {
-      var activeState = $("#menu-container .menu-list").hasClass("active");
-      $("#menu-container .menu-list").animate({
-        right: activeState ? "0%" : "-100%"
-      }, 400);
-    }
-    $("#menu-wrapper").click(function(event) {
-      event.stopPropagation();
-      $("#hamburger-menu").toggleClass("open");
-      $("#menu-container .menu-list").toggleClass("active");
-      slideMenu();
-
-      $("body").toggleClass("overflow-hidden");
-    });
-
-    $(".menu-list").find(".accordion-toggle").click(function() {
-      $(this).next().toggleClass("open").slideToggle("fast");
-      $(this).toggleClass("active-tab").find(".menu-link").toggleClass("active");
-
-      $(".menu-list .accordion-content").not($(this).next()).slideUp("fast").removeClass("open");
-      $(".menu-list .accordion-toggle").not(jQuery(this)).removeClass("active-tab").find(".menu-link").removeClass("active");
-    });
-  }); // jQuery load
 //마우스오버//
 function openMENU(evt, menuName) {
   var i, tabcontent, tablinks;
